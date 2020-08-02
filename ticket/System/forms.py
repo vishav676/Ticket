d8402e5c3f370cc9aacd8e30d25a316e1e4492de
Vishav <vishav@gmail.com>
2020-08-02 17:37:27 +0200

    Small Changes in Frontend

--- ticket/System/forms.py
+++ ticket/System/forms.py
@@ -6,6 +6,23 @@ from django.forms import ModelForm, ModelChoiceField
 
 
 class CreateUser(UserCreationForm):
+    email = forms.EmailField(label="",widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        "placeholder": "Email"
+    }))
+    username = forms.CharField(label="",widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        "placeholder": "username"
+    }))
+    password1 = forms.CharField(label="", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        "placeholder": "Password"
+    }))
+    password2 = forms.CharField(label="", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        "placeholder": "Confirm Password"
+    }))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
