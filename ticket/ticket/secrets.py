3146b13a82b81acae1f67e978189af26132d0430
Vishav Lakhtia <vishav676@gmail.com>
2020-12-12 21:36:39 +0100

    Adding option for Collaboration

---
+++ ticket/ticket/secrets.py
@@ -0,0 +1,6 @@
+DJANGO_SECRET_KEY = "c2l4_bw$4+zyb%4kx9wr$)6u=$fc6^om29018v*9j^bezw*t!s"
+NAME = "ProgressDB"
+USER = "postgres"
+PASSWORD = "70870.vishav"
+PORT = 5432
+
--- ticket/ticket/settings.py
+++ ticket/ticket/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR + "templates")
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["progress-tracker-bugs.herokuapp.com", "127.0.0.1"]
 
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'ticket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': NAME,
-        'USER': USER,
-        'PASSWORD': PASSWORD,
-        'PORT': PORT,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -118,14 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID_SECRET
-AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY_SECRET
-AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME_SECRET
-DEFAULT_FILE_STORAGE = DEFAULT_FILE_STORAGE_SECRET
-STATICFILES_STORAGE = STATICFILES_STORAGE_SECRET
-AWS_S3_FILE_OVERWRITE = AWS_S3_FILE_OVERWRITE_SECRET
-AWS_DEFAULT_ACL = AWS_DEFAULT_ACL_SECRET
-AWS_S3_REGION_NAME = "eu-central-1"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
